9c0493b616b08c2ed59cb950266b63b53153e375
Ali-GreenHeart <elinemet.isiyev@gmail.com>
2025-04-23 16:10:22 +0400

    setup tailwind, shadcn

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import "@/index.css";
 
 createRoot(document.getElementById('root')!)
   .render(
     <StrictMode>
-      <p>salam</p>
+      <p className='text-3xl bg-red-400'>salam</p>
     </StrictMode>,
   )
