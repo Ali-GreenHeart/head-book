ca9e10b576ce2ce7e73c76f15d63f38adf608439
Ali-GreenHeart <elinemet.isiyev@gmail.com>
2025-04-23 15:13:11 +0400

    add tailwindcss, configure aliases (absolute path)

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,24 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import path from "path"
 
-// https://vitejs.dev/config/
+
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   return {
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+        "@@": path.resolve(__dirname, "./src/components")
+      }
+    },
     define: {
       'process.env.REACT_BE_API_URL': JSON.stringify(env.REACT_BE_API_URL)
     },
-    plugins: [react()],
+    plugins: [
+      react(),
+      tailwindcss()
+    ],
   }
 })
